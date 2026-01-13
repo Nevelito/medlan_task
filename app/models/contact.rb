51e4c0f8ccb4311088d4146524b6f894d8716143
Nevelito <szymonnewel@gmail.com>
2026-01-13 16:19:33 +0100

    implement data normalisation and validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,24 @@
 class Contact < ApplicationRecord
+  before_validation :normalize_data
+
+  validates :name, :surname, :email, :category, presence: true
+  validates :email, uniqueness: true
+  validates :phone, format: {
+    with: /\A\d{9}\z/,
+    message: "has to include exact 9 numbers"
+  }
+
   enum :category, { family: 0, friends: 1, work: 2 }
+
+  private
+
+  def normalize_data
+    self.name = normalize(name)
+    self.surname = normalize(surname)
+    self.phone = phone.to_s.gsub(/\D/, "")
+  end
+
+  def normalize(value)
+    value.to_s.strip.capitalize # to_s used in case value is nil
+  end
 end
