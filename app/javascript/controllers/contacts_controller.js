13c9e7ea20f2cb6f4e8744cfdb3066175a7a3d7b
Nevelito <szymonnewel@gmail.com>
2026-01-14 17:20:08 +0100

    add stimulus controller to count rows in Contact table, add targets and actions

---
+++ app/javascript/controllers/contacts_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    static targets = ["count", "list"]
+
+    connect() {
+        this.updateCount()
+    }
+
+    onFrameRender() {
+        this.updateCount()
+    }
+
+    updateCount() {
+        this.countTarget.textContent = this.listTarget.querySelectorAll("tbody tr").length
+    }
+}
