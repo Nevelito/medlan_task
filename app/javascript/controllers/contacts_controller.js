90f26a2b01c32cd31d05ccaa565d6beeb568127e
Nevelito <szymonnewel@gmail.com>
2026-01-15 17:39:07 +0100

    fix issues with broadcasting

--- app/javascript/controllers/contacts_controller.js
+++ app/javascript/controllers/contacts_controller.js
@@ -1,16 +1,29 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ["count", "list"]
+    static targets = ["count", "list", "form", "trigger"]
 
     connect() {
         this.updateCount()
-        this.observer = new MutationObserver(() => this.updateCount())
+        this.observer = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if (this.listTarget.contains(mutation.target)) {
+                    this.updateCount()
+                }
+
+                if (this.triggerTarget.contains(mutation.target)) {
+                    this.onBroadcastAction()
+                }
+            })
+        })
         this.observer.observe(this.element, { childList: true, subtree: true })
     }
 
-    onFrameRender() {
-        this.updateCount()
+    onBroadcastAction() {
+        // TODO increase delay if there will be more users
+        const delay = Math.random() * 100;
+
+        setTimeout(() => { this.formTarget.requestSubmit(); }, delay);
     }
 
     updateCount() {
